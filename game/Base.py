7dc1dcff973f62660379b50e8922106643d19457
guanlin-w <36206304+guanlin-w@users.noreply.github.com>
2024-04-28 02:21:55 -0500

    Added ability hold pieces and swap. Must place a piece before being able  to swap again. Also fixed drop location

--- game/Base.py
+++ game/Base.py
@@ -139,18 +139,28 @@ T = [[
 
 shapes = [S, Z, I, O, J, L, T]
 shape_colors = [(0, 255, 0), (255, 0, 0), (0, 255, 255), (255, 255, 0), (255, 165, 0), (0, 0, 255), (128, 0, 128)]
+
+# the x offset is 3 blocks except for the O piece 
+shape_start = [(3,0),(3,0),(3,0),(4,0),(3,0),(3,0),(3,0)]
 # index 0 - 6 represent shape
 
 
 class Piece(object):
     rows = 20
     columns = 10
-    def __init__(self,column,row,shape):
-        self.x = column
-        self.y = row
+    def __init__(self,shape):
+        index = shapes.index(shape)
+        self.start_pos = shape_start[index]
+
+        self.x = self.start_pos[0]
+        self.y = self.start_pos[1]
         self.shape = shape
-        self.color = shape_colors[shapes.index(shape)]
+        self.color = shape_colors[index]
         self.rotation = 0 # 0-3 determines the rotation
+
+    def reset_position(self):
+        self.x = self.start_pos[0]
+        self.y = self.start_pos[1]
 
 # determines how to draw the grid
 def create_grid(locked_positions={}):
@@ -197,7 +207,7 @@ def check_lost(positions):
 
 def get_shape():
     global shapes, shape_colors
-    return Piece(5,0,random.choice(shapes))
+    return Piece(random.choice(shapes))
 
 def draw_text_middle(surface, text, size, color):
     font = pygame.font.SysFont("comicsans", size, bold=True)
@@ -253,6 +263,27 @@ def draw_next_shape(shapes, surface):
 
     surface.blit(label, (top_left_x + play_width + 25, top_left_y + play_height/2 - 100 - 50))
 
+def draw_hold_shape(shape, surface):
+    font = pygame.font.SysFont('comicsans', 30)
+    label = font.render('Holding', 1, (255,255,255))
+    sx = top_left_x - 150
+    sy = top_left_y + play_height/2 -200
+    if shape:
+
+        format = shape.shape[0]
+        for i, line in enumerate(format):
+            row = list(line)
+            for j, column in enumerate(row):
+                if column == '0':
+                    pygame.draw.rect(surface, shape.color, (sx + j*30, sy + i*30, 30, 30), 0)
+
+    # sx = top_left_x + play_width + 25
+    # sy = top_left_y + play_height/2 - 400
+
+    # surface.blit(label, (sx + 20, sy + 160))
+    surface.blit(label, (sx, top_left_y + play_height/2 - 260))
+
+
 def draw_window(surface, grid, score=0, last_score = 0):
     surface.fill((0, 0, 0))
 
@@ -295,10 +326,14 @@ def main(win):
     run = True
     current_piece = get_shape()
     next_piece = [get_shape() for _ in range(look_ahead)]
+    hold_piece = None
     clock = pygame.time.Clock()
     fall_time = 0
     level_time = 0
     score = 0
+
+    # can only initiate swap if a piece has been placed
+    swap = False
     while run:
 
         level_time += clock.get_rawtime()
@@ -342,12 +377,23 @@ def main(win):
                     if not valid_space(current_piece, grid):
                         current_piece.rotation = current_piece.rotation - 1 % len(current_piece.shape)
  
-                if event.key == pygame.K_DOWN:
+                elif event.key == pygame.K_DOWN:
                     # move shape down
                     current_piece.y += 1
                     if not valid_space(current_piece, grid):
                         current_piece.y -= 1
-
+                if event.key == pygame.K_c and not swap:
+                    # handle piece change if necessary
+                    # handle the first swap
+                    temp_piece = hold_piece
+                    hold_piece = current_piece
+                    if temp_piece is not None:
+                        current_piece = temp_piece
+                    else:
+                        current_piece = next_piece.pop(0)
+                        next_piece.append(get_shape())
+                    hold_piece.reset_position() #reset to top
+                    swap = True
         shape_pos = convert_shape_format(current_piece)
 
         # add color of piece to the grid for drawing
@@ -355,6 +401,9 @@ def main(win):
             x, y = shape_pos[i]
             if y > -1: # If we are not above the screen
                 grid[y][x] = current_piece.color
+
+
+
         # IF PIECE HIT GROUND
         if change_piece:
             for pos in shape_pos:
@@ -364,8 +413,12 @@ def main(win):
             next_piece.append(get_shape())
             change_piece = False
             score += clear_rows(grid,locked_positions)*10
+            swap = False
+
+
         draw_window(win,grid,score,0)
         draw_next_shape(next_piece, win)
+        draw_hold_shape(hold_piece,win)
         pygame.display.update()
         if check_lost(locked_positions):
             draw_text_middle(win, "YOU LOST!", 80, (255,255,255))
